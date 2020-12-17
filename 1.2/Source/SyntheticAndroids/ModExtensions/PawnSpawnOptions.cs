e7ca3f12b71c02e2b410d7732b302be95c5af519
Taranchuk <nfhfyxer@gmail.com>
2020-12-17 17:05:41 +0200

    update

---
+++ 1.2/Source/SyntheticAndroids/ModExtensions/PawnSpawnOptions.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace SyntheticAndroids
+{
+    public class PawnSpawnOptions : DefModExtension
+    {
+        public List<TraitDef> disallowedTraits;
+    }
+}
